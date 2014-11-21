d1207bbfdfdf8b5f8f4162358ebd22a62c13a68a
dobro <dobro@localhost>
2014-11-21 04:11:48 +0200

    Поправки

--- views/packaging/_form.php
+++ views/packaging/_form.php
@@ -21,7 +21,7 @@ use yii\helpers\ArrayHelper;
     
     <?= $form->field($model, 'category_id')->dropDownList(ArrayHelper::map(Category::find()->all(), 'category_id', 'category_title'),[]) ?>
     
-    <?php/*= $form->field($model, 'packaging_is_additional')->checkbox() */?>
+    
 
     
     <?php
