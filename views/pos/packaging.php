bd2ae2521a6a6c4d80cd6d894d2c80132a35c1a1
dobro <dobro@localhost>
2015-01-08 22:20:46 +0200

    -

--- views/pos/packaging.php
+++ views/pos/packaging.php
@@ -45,6 +45,12 @@ $this->params['breadcrumbs'][] = $this->title;
                                 return '<nobr>'.Html::textInput('pos_packaging_price', $model['pos_packaging_price'], ['class'=>'pos_packaging_price form-control','data-packaging-id'=>$model['packaging_id'],'data-pos-id'=>$pos->pos_id,'size'=>3]).'&nbsp;'.\Yii::$app->params['currency'].'</nobr>';
                            }
             ], 
+            [
+               'label'=>\Yii::t('app','pos_packaging_visible'),
+               'content'=>function ($model, $key, $index, $column) use($pos){
+                                return '<nobr>'.Html::checkbox('pos_packaging_visible', $model['pos_packaging_visible'], ['class'=>'pos_packaging_visible form-control','data-packaging-id'=>$model['packaging_id'],'data-pos-id'=>$pos->pos_id]).'</nobr>';
+                           }
+            ], 
         ],
     ]); ?>
     </div>
@@ -77,7 +83,22 @@ $this->params['breadcrumbs'][] = $this->title;
                 success: function (response) {
                 }
             });
-
+        }
+        
+        function update_pos_packaging_visible(packaging_id, pos_id, pos_packaging_visible){
+            $.ajax({
+                type: 'POST',
+                cache: false,
+                dataType:'json',
+                url: '" . Url::toRoute(['/pospackaging/updatevisibility']) . "',
+                data:{
+                  packaging_id:packaging_id,
+                  pos_id:pos_id,
+                  pos_packaging_visible:pos_packaging_visible
+                },
+                success: function (response) {
+                }
+            });
         }
         function activateForm(){
             $('.pos_packaging_price').change(function(event){
@@ -87,6 +108,14 @@ $this->params['breadcrumbs'][] = $this->title;
                 var pos_packaging_price=ele.val();
                 update_pos_price(packaging_id, pos_id, pos_packaging_price);
             });
+            $('.pos_packaging_visible').change(function(event){
+                var ele=$(event.target);
+                var packaging_id=ele.attr('data-packaging-id');
+                var pos_id=ele.attr('data-pos-id');
+                var pos_packaging_visible=ele.is(':checked')?1:0;
+                update_pos_packaging_visible(packaging_id, pos_id, pos_packaging_visible);
+            });            
+
             $('#tablefilter').keyup(function(){
                var filter=$('#tablefilter').val().toLowerCase();
                $('#packTable tbody tr').each(function(key, val){
